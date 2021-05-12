8a5bc9549ea94307e0470fa9942146557a1f4e96
mhubert-pmn <82522321+mhubert-pmn@users.noreply.github.com>
2021-05-12 12:26:17 +0200

    modified for verification user with bcrypt

---
+++ back_docker_node_tp/src/controllers/userController.js
@@ -0,0 +1,82 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.userRegister = (req, res) => {
+    let newUser = new User(req.body);
+    newUser.save((error, user) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            });
+        } else {          
+            res.status(201);
+            res.json({
+                message: `Utilisateur crée : ${user.email}`,
+            });
+        }
+    });
+}
+
+exports.userLogin = (req, res) => {
+    // Rechercher l'utilisateur
+    User.findOne({
+        email: req.body.email
+    }, (error, user) => {
+        // Si l'utilisateur n'est pas trouvé
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            });
+        }
+        // Si l'utilisateur est trouvé
+        else {
+            // Si l'email et le mot de passe correspondent
+            if (user != null) {
+                // Compare le mot de passe stocké à celui entré en décryptant
+                if (user.email === req.body.email &&  bcrypt.compare(req.body.password, user.password)) {
+                    jwt.sign({
+                        user : {
+                            id: user._id,
+                            email: user.email,
+                            admin: user.admin
+                        }
+                    }, process.env.JWT_KEY, {
+                        expiresIn: "30 days"
+                    }, (error, token) => {
+                        if (error) {
+                            res.status(500);
+                            console.log(error);
+                            res.json({
+                                message: "Erreur serveur."
+                            });
+                        } else {
+                            res.status(200);
+                            res.json({
+                                token
+                            });
+                        }
+                    })
+                } else {
+                    res.status(403);
+                    console.log(error);
+                    res.json({
+                        message: "Authentification incorrect."
+                    });
+                }
+            }
+            // Si l'email et le mot de passe ne correspondent pas
+            else {
+                res.status(403);
+                console.log(error);
+                res.json({
+                    message: "Authentification incorrect."
+                });
+            }
+        }
+    });
+}
